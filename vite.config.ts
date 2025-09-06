61438e5061711042fc230200b8b09811e4e60774
Rajat Sharma <merajatsharma@outlook.com>
2025-09-07 01:45:24 +0530

    delpoy

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    base: '/weather-frontend/',
+    base: './weather-frontend/',
     plugins: [react()],
     server: {
       proxy: {
