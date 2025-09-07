d0b1122820a455bda8f81a8f788838a0175824a5
Rajat Sharma <merajatsharma@outlook.com>
2025-09-07 14:06:24 +0530

    url update.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,7 +43,7 @@ const Header: React.FC<HeaderProps> = ({ onMenuToggle, user, onLogout }) => {
   return (
     <AppBar
       position="fixed"
-      sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, height: "64px" }}
     >
       <Toolbar>
         {isMobile && (
